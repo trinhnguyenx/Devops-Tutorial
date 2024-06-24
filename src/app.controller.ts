ffa61aa251090672901fd2b4e0ad6fd3c08ba8e5
CongTrinhh <96908827+trinhnguyenx@users.noreply.github.com>
2024-06-24 19:14:16 +0700

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello, world!777';
+    return 'Hello, world!7779999';
   }
 }
