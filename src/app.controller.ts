d1221b61aa06e34ff4669421394d4936bab2c0f3
CongTrinhh <nguyencongtrinhqb@gmail.com>
2024-07-29 19:06:12 +0700

    chore: change api health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Response } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   getHello(): string {
     return 'Hello, world!77797777999';
+  }
+  @Get('health')
+  getHealth(@Response() res : any, req: string): string {
+    return res.send('hello, world!');
   }
 }
