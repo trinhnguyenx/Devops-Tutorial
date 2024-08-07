9d10beb902509d3ede31760ed1fec297325b9aa0
CongTrinhh <nguyencongtrinhqb@gmail.com>
2024-08-07 22:11:47 +0700

    fix: fix code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get, Response } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello, world!lalal';
+    return 'Hello, world!laslsndlkafnckc';
   }
 }
